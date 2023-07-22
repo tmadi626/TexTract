8e1d7fca57261924c57242fd9a4079d032fa571c
tmadi626 <tahaomadi@gmail.com>
2023-07-21 22:10:08 -0230

    Setting up the project workspace

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-# Main entry point of the program
+import configparser
+from my_module import my_program
+
+if __name__ == "__main__":
+    # Read the configuration file
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    # Pass the configuration to the main program
+    my_program.run(config)
---
+++ my_module/my_program.py
@@ -0,0 +1,8 @@
+# my_module/my_program.py
+def run(config):
+    program_name = config.get("General", "ProgramName")
+    # param1 = config.get("General", "Parameter1")
+    # param2 = config.get("General", "Parameter2")
+
+    # Your program logic here, using the program_name, param1, and param2
+    # ...
